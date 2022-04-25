c3b7abd5beab8bd743e776f5a67f451171fe4975
Andrey <andreybr2002@hotmail.com>
2022-04-25 20:01:00 -0300

    Implementada máscara de moeda e número de telefone

--- mascaras-form.js
+++ mascaras-form.js
@@ -38,6 +38,7 @@ function cpfCnpj(v) {
     return v
 }
 
+//Máscara do campo Data
 var input = document.querySelectorAll('.input-data')[0];
 var input2 = document.querySelectorAll('.input-bom-para')[0];
 var input3 = document.querySelectorAll('.input-data-td')[0];
@@ -69,3 +70,47 @@ var dateInputMask = function dateInputMask(elm) {
 dateInputMask(input);
 dateInputMask(input2);
 dateInputMask(input3);
+
+
+var campoInput = document.getElementById("valor-recibo");
+//Máscara do campo Valor do Recibo
+function formatarMoeda() {
+    let valor = campoInput.value;
+
+    if (campoInput.value.length > 2) {
+        valor = parseInt(valor.replace(/[\D]+/g, ""));
+        valor = valor + "";
+
+        valor = valor.replace(/([0-9]{2})$/g, ",$1");
+
+        campoInput.value = valor;
+        if (valor == "NaN") campoInput.value = "";
+
+        if (campoInput.value.charAt(0) == ",") {
+            campoInput.value = "0" + campoInput.value;
+        }
+    }
+}
+
+function addZero() {
+    if (campoInput.value.length < 3 && campoInput.value.charAt(0) !== ",") {
+        campoInput.value = campoInput.value + ",00";
+    }
+
+    if (campoInput.value.charAt(0) == ",") {
+        campoInput.value = "0" + campoInput.value;
+    }
+}
+
+campoInput.addEventListener("keyup", formatarMoeda);
+campoInput.addEventListener("blur", addZero);
+
+//Máscara do campo telefone
+const phone = document.getElementById('numero-prestador');
+phone.addEventListener('input', function (e) {
+    let arr = this.value.replace(/[^\dA-Z]/g, '').replace(/[\s-)(]/g, '').split('');
+    if (arr.length >= 1) arr.splice(0, 0, '(');
+    if (arr.length > 3) arr.splice(3, 0, ') ');
+    if (arr.length > 9) arr.splice(9, 0, '-');
+    this.value = arr.toString().replace(/[,]/g, '');
+});
